bc09c5068e225cf015512f3fd5ac5d09df2c9c4e
Yu Huang <yuhuang.toronto@gmail.com>
2020-10-08 22:50:06 -0400

    init v1

---
+++ model/model.go
@@ -0,0 +1,32 @@
+package model
+
+import (
+	"time"
+)
+
+type OpenEventContent struct {
+	Balance int
+}
+
+type TransferEventContent struct {
+	Amount int
+	FromID string
+	ToID   string
+}
+
+type StoreEventContent struct {
+	Amount int
+}
+
+type WithdrawEventContent struct {
+	Amount int
+}
+
+type Event struct {
+	ID       int
+	Type     string
+	Entity   string
+	EntityID string
+	Content  interface{}
+	Time     time.Time
+}
